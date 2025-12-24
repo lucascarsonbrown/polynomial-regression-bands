b94788919f458f32224e97f9318e11dc5723fce5
lucascarsonbrown <lucascarsonbrown@gmail.com>
2025-12-24 14:00:49 -0500

    edits

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-import matplotlib
+# import matplotlib
 from datetime import datetime
 from utils import run_full_analysis, calculate_metrics, calculate_yearly_returns
 
